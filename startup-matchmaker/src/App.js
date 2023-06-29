752e77fc455a59694329eee0ccf7a07d12a32e47
bragceo <119948453+bragceo@users.noreply.github.com>
2023-06-29 19:29:42 -0400

    set up routes for different pages using react-router-dom

--- startup-matchmaker/src/App.js
+++ startup-matchmaker/src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
+import Homepage from './components/HomePage'; // need to add
+import ProfilePage from './components/ProfileForm';
+import StartupPage from './components/StartupCard';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/">
+            <HomePage />
+          </Route>
+          <Route path="/profile">
+            <ProfilePage />
+          </Route>
+          <Route path="/startup/:id">
+            <StartupPage />
+          </Route>
+          {/* Add more <Route> tags as needed for additional pages */}
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
