15b4e49d0bdbef12f5b92ca1013fed5daa010ef2
Whirlwindraven <fadysherif1998@gmail.com>
2023-07-04 10:12:35 -0400

    created an investment schema

--- startup-matchmaker/server/models/Investment.js
+++ startup-matchmaker/server/models/Investment.js
@@ -1,1 +1,17 @@
 //record of the transaction through stripe finalizing the payment from the investor to the startup
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const investmentSchema = new Schema({
+  investor: { type: mongoose.Types.ObjectId, ref: 'Investor' },
+  startup: { type: mongoose.Types.ObjectId, ref: 'Startup' },
+  stripeChargeId: String,
+  status: String,
+  amount: Number,
+  currency: String,
+  description: String,
+  timestamp: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model('Investment', investmentSchema);
+
