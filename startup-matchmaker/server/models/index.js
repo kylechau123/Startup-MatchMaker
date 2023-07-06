a2ac842cfe37c610149ac6e74a336ba60f4af360
Whirlwindraven <fadysherif1998@gmail.com>
2023-07-05 21:12:33 -0400

    added data model to index

--- startup-matchmaker/server/models/index.js
+++ startup-matchmaker/server/models/index.js
@@ -2,5 +2,6 @@ const Investor = require ('./Investor');
 const Startup = require ('./Startup');
 const Investment = require('./Investment');
 const Conversation = require('./Conversation');
+const Message = require ('./Message')
 
-module.exports = { Investor, Startup, Investment, Conversation};
+module.exports = { Investor, Startup, Investment, Conversation, Message};
