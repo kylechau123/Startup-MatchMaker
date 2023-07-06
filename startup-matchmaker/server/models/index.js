75f06dabd23323b2fc73d61504b1a7164a84fd5c
Whirlwindraven <fadysherif1998@gmail.com>
2023-07-05 20:43:56 -0400

    created a conversation model

--- startup-matchmaker/server/models/index.js
+++ startup-matchmaker/server/models/index.js
@@ -1,5 +1,6 @@
 const Investor = require ('./Investor');
 const Startup = require ('./Startup');
 const Investment = require('./Investment');
+const Conversation = require('./Conversation');
 
-module.exports = { Investor, Startup, Investment };
+module.exports = { Investor, Startup, Investment, Conversation};
