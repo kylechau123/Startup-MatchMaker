4f2e7e62330e6a9a5fa3a58fb89aa06f1691397f
bragceo <119948453+bragceo@users.noreply.github.com>
2023-07-06 19:54:59 -0400

    Implement the logic to handle form submissions in the ProfileForm component

--- startup-matchmaker/src/components/ProfileForm.js
+++ startup-matchmaker/src/components/ProfileForm.js
@@ -1,14 +1,65 @@
-import React from "react";
-import { Form, Button } from "react-bootstrap";
+import React, { useState, useEffect } from "react";
+import ProfileForm from "./ProfileForm";
+import axios from "axios";
+import { gql } from "graphql-tag";
 
-const ProfileForm = ({ onSubmit }) => (
-  <Form onSubmit={onSubmit}>
-    <Form.Control type="text" placeholder="Name" />
-    <Form.Control type="email" placeholder="Email" />
-    <Form.Control as="textarea" placeholder="Bio" />
-    <Button type="submit">Save</Button>
-  </Form>
-);
+const ProfilePage = () => {
+  const [profileData, setProfileData] = useState({});
 
-export default ProfileForm;
+  useEffect(() => {
+    // Fetch profile data from the backend API and set it to state
+    const query = gql`
+      query Profile {
+        profile {
+          name
+          email
+          bio
+        }
+      }
+    `;
+    axios.get("/api/graphql", {
+      params: {
+        query,
+      },
+    }).then((response) => {
+      setProfileData(response.data.data.profile);
+    });
+  }, []);
+
+  const updateProfileData = async (formData) => {
+    try {
+      const query = gql`
+        mutation UpdateProfile($name: String!, $email: String!, $bio: String!) {
+          updateProfile(name: $name, email: $email, bio: $bio) {
+            name
+            email
+            bio
+          }
+        }
+      `;
+      const response = await axios.post("/api/graphql", {
+        params: {
+          query,
+          variables: {
+            name: formData.name,
+            email: formData.email,
+            bio: formData.bio,
+          },
+        },
+      });
+      setProfileData(response.data.data.updateProfile);
+    } catch (error) {
+      console.log("Error updating profile:", error);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Profile Page</h1>
+      <ProfileForm onSubmit={updateProfileData} profileData={profileData} />
+    </div>
+  );
+};
+
+export default ProfilePage;
 
