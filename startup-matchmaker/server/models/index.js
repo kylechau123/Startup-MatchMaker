c5d8f46c49428f70872a6a2c18ea0939067965cb
kylechau123 <123038673+kylechau123@users.noreply.github.com>
2023-07-08 14:56:06 -0400

    moved client side files into seperatre folder from the server folder

--- startup-matchmaker/server/models/index.js
+++
@@ -1,7 +0,0 @@
-const Investor = require ('./Investor');
-const Startup = require ('./Startup');
-const Investment = require('./Investment');
-const Conversation = require('./Conversation');
-const Message = require ('./Message')
-
-module.exports = { Investor, Startup, Investment, Conversation, Message};
