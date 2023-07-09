f23b6cf055c9e8222d41dc87849b0b6afbdcc43c
Whirlwindraven <fadysherif1998@gmail.com>
2023-07-09 12:13:36 -0400

    fixed issue with server.js because it wasn't requiring the files in the schema dir

--- startup-matchmaker/server/server.js
+++ startup-matchmaker/server/server.js
@@ -2,8 +2,10 @@ const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 const { authMiddleware } = require('./utils/auth');
+const { typeDefs } = require('./schemas/typeDefs');
+const { resolvers } = require('./schemas/resolvers');
 
-const { typeDefs, resolvers } = require('./schemas');
+
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
