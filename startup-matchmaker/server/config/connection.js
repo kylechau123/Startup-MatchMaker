d8c20247f254b82c1db198af06a9a42f8a719c2b
Whirlwindraven <fadysherif1998@gmail.com>
2023-07-09 13:10:49 -0400

    changed db name

--- startup-matchmaker/server/config/connection.js
+++ startup-matchmaker/server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/your-db-name");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/startup-matchmaker");
 
 mongoose.connection.on('connected', () => {
     console.log('Mongoose is connected')
