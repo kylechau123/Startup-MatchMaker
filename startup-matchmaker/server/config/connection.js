17484697283b574576e3fffa5a70aac899b69ed8
Whirlwindraven <fadysherif1998@gmail.com>
2023-07-09 13:10:12 -0400

    re did the connection.js again

--- startup-matchmaker/server/config/connection.js
+++ startup-matchmaker/server/config/connection.js
@@ -1,9 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.set('useNewUrlParser', true);
-mongoose.set('useUnifiedTopology', true);
-
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/your-db-name", {});
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/your-db-name");
 
 mongoose.connection.on('connected', () => {
     console.log('Mongoose is connected')
