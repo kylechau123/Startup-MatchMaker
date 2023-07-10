df6a41ca742a90f197c6df0a10b2dcdda7c15a14
kylechau123 <123038673+kylechau123@users.noreply.github.com>
2023-07-10 17:14:39 -0400

    organizing all branches

---
+++ client/src/components/Notification.js
@@ -0,0 +1,21 @@
+import { Card, CardBody, Text, Button } from "@chakra-ui/react";
+import { IoIosNotifications } from 'react-icons/io';
+import { Link } from 'react-router-dom';
+
+const Notification = (props) => {
+    const { message, link, createdAt } = props.notification;
+    return (
+        <Link to={link}>
+            <Card borderRadius="md"
+                boxShadow="md"
+                transition="transform 0.3s" _hover={{ transform: 'scale(1.05)' }} mb={3}>
+                <CardBody display="flex" gap={5} alignItems="center" justifyContent="space-between">
+                    <IoIosNotifications style={{ fontSize: '22px' }} /> <Text fontSize="md"> {message} </Text>
+                    <Text fontSize="sm" color="gray.500" alignSelf="flex-end"> {new Date(createdAt).toLocaleString()} </Text>
+                </CardBody>
+            </Card>
+        </Link>
+    );
+}
+
+export default Notification;
